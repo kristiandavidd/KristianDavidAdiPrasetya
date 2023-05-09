8f10e524b92a426a0788b07c4456f02496cf7ebd
Kristian David <67230500+kristiandavidd@users.noreply.github.com>
2023-05-09 07:06:35 +0700

    Update PolimorfismeCoercion.java

--- praktikum_7/PolimorfismeCoercion.java
+++ praktikum_7/PolimorfismeCoercion.java
@@ -1,3 +1,8 @@
+// Nama file    : PolimorfismeCoercion.java
+// tanggal      : 9 Mei 2023
+// Nama/NIM     : Kristian David Adi Prasetya/24060121130049
+// Deskripsi    : Kelas Polimorfisme bertipe Coercion
+
 package praktikum_7;
 
 public class PolimorfismeCoercion {
