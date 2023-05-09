d337580e1f12b16681e73c33ef99c00b775a2fb0
Kristian David <67230500+kristiandavidd@users.noreply.github.com>
2023-05-09 07:01:15 +0700

    Update Bus.java

--- praktikum_7/Bus.java
+++ praktikum_7/Bus.java
@@ -1,3 +1,8 @@
+// Nama file    : Bus.java
+// tanggal      : 9 Mei 2023
+// Nama/NIM     : Kristian David Adi Prasetya/24060121130049
+// Deskripsi    : Kelas Bus yang merupakan extends dari Vehicle
+
 package praktikum_7;
 
 public class Bus extends Vehicle{
