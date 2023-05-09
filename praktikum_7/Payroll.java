507814514edd710d8bb56d64dd38f612579ce40d
Kristian David <67230500+kristiandavidd@users.noreply.github.com>
2023-05-09 07:03:27 +0700

    Update Payroll.java

--- praktikum_7/Payroll.java
+++ praktikum_7/Payroll.java
@@ -1,3 +1,8 @@
+// Nama file    : Payroll.java
+// tanggal      : 9 Mei 2023
+// Nama/NIM     : Kristian David Adi Prasetya/24060121130049
+// Deskripsi    : Kelas Payroll untuk menerapkan Polimorfisme
+
 package praktikum_7;
 
 public class Payroll {
